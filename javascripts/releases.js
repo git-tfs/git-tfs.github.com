6e2e9d00ba62fcda93af5a5913af57bbac3ac5f6
Matt Burke <spraints@gmail.com>
2013-02-20 10:15:35 -0500

    Show a spinner while fetching the latest release.

--- javascripts/releases.js
+++ javascripts/releases.js
@@ -1,9 +1,12 @@
 $(function() {
-  var downloadContainer = $('.js-download-container');
+  var spinner = new Spinner({color: '#444', radius: 5, length: 4, width: 2, top: 0});
+  var downloadContainer = $('.js-download-container').each(function() { spinner.spin(this); });
   if(downloadContainer.length > 0) {
     $.ajax({
       url: 'http://github.dev/git-tfs/git-tfs/releases.atom',
-      error: function() { console.log(arguments); },
+      complete: function() {
+        spinner.stop();
+      },
       success: function(data) {
         var feed = $(data);
         var mostRecentBinary = feed.find('entry link[rel=enclosure]').first();
